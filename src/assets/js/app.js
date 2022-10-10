46e23ce3652a1bc94d290e18287490f6fcee2296
Alexmin22 <yashi80@list.ru>
2022-10-10 19:34:53 +0300

    1

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,13 @@
+$(document).ready(function() {
+  $("#catalogue").click(function() {
+  $("#menu").slideToggle();
+  });
+  });
+
+  $(function(){
+    $('.catalogue__btn').on('click', function(){
+        $('.sprite--chevron').toggleClass('sprite--up');
+    });
+});
+
 console.log('file 1');
